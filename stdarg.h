5c81896e06ae72da7a976c2ebf808fffb01bd430
bellard <bellard>
2002-08-31 12:43:53 +0000

    double fix

--- stdarg.h
+++ stdarg.h
@@ -4,9 +4,8 @@
 typedef char *va_list;
 
 /* only correct for i386 */
-/* XXX: incorrect for type size different than 4 bytes*/
-#define va_start(ap,last) ap = ((char *)&(last)) + sizeof(int); 
-#define va_arg(ap,type) (ap += sizeof(int), *(type *)(ap - sizeof(int)))
+#define va_start(ap,last) ap = ((char *)&(last)) + ((sizeof(last)+3)&~3)
+#define va_arg(ap,type) (ap += (sizeof(type)+3)&~3, *(type *)(ap - ((sizeof(type)+3)&~3)))
 #define va_end(ap)
 
 /* fix a buggy dependency on GCC in libio.h */
