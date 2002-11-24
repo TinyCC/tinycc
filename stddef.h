5fac37b0a28879a489dcd832d49b1e220fced3e9
bellard <bellard>
2002-11-24 15:59:03 +0000

    use TCC macros

--- stddef.h
+++ stddef.h
@@ -2,9 +2,9 @@
 #define _STDDEF_H
 
 #define NULL ((void *)0)
-typedef unsigned int size_t;
-typedef int wchar_t;
-typedef int ptrdiff_t;
+typedef __SIZE_TYPE__ size_t;
+typedef __WCHAR_TYPE__ wchar_t;
+typedef __PTRDIFF_TYPE__ ptrdiff_t;
 #define offsetof(type, field) ((size_t) &((type *)0)->field)
 
 /* need to do that because of glibc 2.1 bug (should have a way to test
