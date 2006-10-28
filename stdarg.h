84f4e9963569f930feaa536d03f98c448cb1d75c
bellard <bellard>
2006-10-28 19:45:50 +0000

    added va_copy()

--- stdarg.h
+++ stdarg.h
@@ -6,6 +6,7 @@ typedef char *va_list;
 /* only correct for i386 */
 #define va_start(ap,last) ap = ((char *)&(last)) + ((sizeof(last)+3)&~3)
 #define va_arg(ap,type) (ap += (sizeof(type)+3)&~3, *(type *)(ap - ((sizeof(type)+3)&~3)))
+#define va_copy(dest, src) (dest) = (src)
 #define va_end(ap)
 
 /* fix a buggy dependency on GCC in libio.h */
