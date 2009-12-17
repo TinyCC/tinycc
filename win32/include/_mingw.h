d79b867d551510a64c4074c78d5737be16347fcc
grischka <grischka>
2009-12-17 18:44:58 +0100

    win64: adjust for two args with setjmp(buf,ctx)

--- win32/include/_mingw.h
+++ win32/include/_mingw.h
@@ -57,6 +57,8 @@ typedef unsigned __int64 uintptr_t;
 #define __stdcall
 #define _AMD64_ 1
 #define __x86_64 1
+#define USE_MINGW_SETJMP_TWO_ARGS
+#define mingw_getsp tinyc_getsp
 #else
 typedef __int32 intptr_t;
 typedef unsigned __int32 uintptr_t;
