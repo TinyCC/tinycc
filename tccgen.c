fdf9fba5785f5c0d285c8cbf2fa51df32ddd878d
Archidemon <_dangerdl@mail.ru>
2014-01-10 11:58:16 +0600

    Fixes previous fixes

--- tccgen.c
+++ tccgen.c
@@ -3909,9 +3909,9 @@ ST_FUNC void unary(void)
         if (vtop->r & VT_SYM) {
             vtop->sym = s;
 #ifdef TCC_TARGET_X86_64
-            s1->vtop->c.ull = 0;
+            vtop->c.ull = 0;
 #else
-            s1->vtop->c.ul = 0;
+            vtop->c.ul = 0;
 #endif
         }
         break;
@@ -5127,10 +5127,10 @@ static void init_putv(CType *type, Section *sec, unsigned long c,
             *(long long *)ptr |= (vtop->c.ll & bit_mask) << bit_pos;
             break;
         case VT_PTR:
-            if (s1->vtop->r & VT_SYM) {
-                greloc(s1, sec, s1->vtop->sym, c, R_DATA_PTR);
+            if (vtop->r & VT_SYM) {
+                greloc(sec, vtop->sym, c, R_DATA_PTR);
             }
-            *(addr_t *)ptr |= (s1->vtop->c.ull & bit_mask) << bit_pos;
+            *(addr_t *)ptr |= (vtop->c.ull & bit_mask) << bit_pos;
             break;
         default:
             if (vtop->r & VT_SYM) {
