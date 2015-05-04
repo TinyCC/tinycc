c75d0deecf6823421e088ec252a9d51445d013a0
seyko <seyko2@gmail.com>
2015-05-04 04:19:24 +0300

    VLA code minor fix

--- x86_64-gen.c
+++ x86_64-gen.c
@@ -2225,6 +2225,8 @@ ST_FUNC void gen_vla_sp_restore(int addr) {
 
 /* Subtract from the stack pointer, and push the resulting value onto the stack */
 ST_FUNC void gen_vla_alloc(CType *type, int align) {
+    int r;
+    get_flags();
 #ifdef TCC_TARGET_PE
     /* alloca does more than just adjust %rsp on Windows */
     vpush_global_sym(&func_old_type, TOK_alloca);
@@ -2232,7 +2234,6 @@ ST_FUNC void gen_vla_alloc(CType *type, int align) {
     gfunc_call(1);
     vset(type, REG_IRET, 0);
 #else
-    int r;
     r = gv(RC_INT); /* allocation size */
     /* sub r,%rsp */
     o(0x2b48);
