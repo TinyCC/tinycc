c497f9c82c5ac218fc9835a74020c247a04a4735
seyko <seyko2@gmail.com>
2015-05-10 10:13:35 +0300

    warn if multile -o option is given

--- libtcc.c
+++ libtcc.c
@@ -1936,6 +1936,10 @@ PUB_FUNC int tcc_parse_args(TCCState *s, int argc, char **argv)
             s->option_m = tcc_strdup(optarg);
             break;
         case TCC_OPTION_o:
+            if (s->outfile) {
+                tcc_warning("multiple -o option");
+                tcc_free(s->outfile);
+            }
             s->outfile = tcc_strdup(optarg);
             break;
         case TCC_OPTION_r:
