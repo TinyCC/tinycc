9bf0e57509b642d6efbad361e8f8c8d676d90db6
Edmund Grimley Evans <Edmund.Grimley.Evans@gmail.com>
2015-11-04 20:20:26 +0000

    tests/tcctest.c: Fix up format strings.

--- tests/tcctest.c
+++ tests/tcctest.c
@@ -173,64 +173,64 @@ int qq(int x)
 void intdiv_test(void)
 {
     printf("18/21=%u\n", 18/21);
-    printf("18%21=%u\n", 18%21);
+    printf("18%%21=%u\n", 18%21);
     printf("41/21=%u\n", 41/21);
-    printf("41%21=%u\n", 41%21);
+    printf("41%%21=%u\n", 41%21);
     printf("42/21=%u\n", 42/21);
-    printf("42%21=%u\n", 42%21);
+    printf("42%%21=%u\n", 42%21);
     printf("43/21=%u\n", 43/21);
-    printf("43%21=%u\n", 43%21);
+    printf("43%%21=%u\n", 43%21);
     printf("126/21=%u\n", 126/21);
-    printf("12%/21=%u\n", 126%21);
+    printf("126%%21=%u\n", 126%21);
     printf("131/21=%u\n", 131/21);
-    printf("131%21=%u\n", 131%21);
+    printf("131%%21=%u\n", 131%21);
     printf("(UINT_MAX/2+3)/2=%u\n", (UINT_MAX/2+3)/2);
-    printf("(UINT_MAX/2+3)%2=%u\n", (UINT_MAX/2+3)%2);
+    printf("(UINT_MAX/2+3)%%2=%u\n", (UINT_MAX/2+3)%2);
 
     printf("18/-21=%u\n", 18/-21);
-    printf("18%-21=%u\n", 18%-21);
+    printf("18%%-21=%u\n", 18%-21);
     printf("41/-21=%u\n", 41/-21);
-    printf("41%-21=%u\n", 41%-21);
+    printf("41%%-21=%u\n", 41%-21);
     printf("42/-21=%u\n", 42/-21);
-    printf("42%-21=%u\n", 42%-21);
+    printf("42%%-21=%u\n", 42%-21);
     printf("43/-21=%u\n", 43/-21);
-    printf("43%-21=%u\n", 43%-21);
+    printf("43%%-21=%u\n", 43%-21);
     printf("126/-21=%u\n", 126/-21);
-    printf("12%/-21=%u\n", 126%-21);
+    printf("126%%-21=%u\n", 126%-21);
     printf("131/-21=%u\n", 131/-21);
-    printf("131%-21=%u\n", 131%-21);
+    printf("131%%-21=%u\n", 131%-21);
     printf("(UINT_MAX/2+3)/-2=%u\n", (UINT_MAX/2+3)/-2);
-    printf("(UINT_MAX/2+3)%-2=%u\n", (UINT_MAX/2+3)%-2);
+    printf("(UINT_MAX/2+3)%%-2=%u\n", (UINT_MAX/2+3)%-2);
 
     printf("-18/21=%u\n", -18/21);
-    printf("-18%21=%u\n", -18%21);
+    printf("-18%%21=%u\n", -18%21);
     printf("-41/21=%u\n", -41/21);
-    printf("-41%21=%u\n", -41%21);
+    printf("-41%%21=%u\n", -41%21);
     printf("-42/21=%u\n", -42/21);
-    printf("-42%21=%u\n", -42%21);
+    printf("-42%%21=%u\n", -42%21);
     printf("-43/21=%u\n", -43/21);
-    printf("-43%21=%u\n", -43%21);
+    printf("-43%%21=%u\n", -43%21);
     printf("-126/21=%u\n", -126/21);
-    printf("-12%/21=%u\n", -126%21);
+    printf("-126%%21=%u\n", -126%21);
     printf("-131/21=%u\n", -131/21);
-    printf("-131%21=%u\n", -131%21);
+    printf("-131%%21=%u\n", -131%21);
     printf("-(UINT_MAX/2+3)/2=%u\n", (0-(UINT_MAX/2+3))/2);
-    printf("-(UINT_MAX/2+3)%2=%u\n", (0-(UINT_MAX/2+3))%2);
+    printf("-(UINT_MAX/2+3)%%2=%u\n", (0-(UINT_MAX/2+3))%2);
 
     printf("-18/-21=%u\n", -18/-21);
-    printf("-18%-21=%u\n", -18%-21);
+    printf("-18%%-21=%u\n", -18%-21);
     printf("-41/-21=%u\n", -41/-21);
-    printf("-41%-21=%u\n", -41%-21);
+    printf("-41%%-21=%u\n", -41%-21);
     printf("-42/-21=%u\n", -42/-21);
-    printf("-42%-21=%u\n", -42%-21);
+    printf("-42%%-21=%u\n", -42%-21);
     printf("-43/-21=%u\n", -43/-21);
-    printf("-43%-21=%u\n", -43%-21);
+    printf("-43%%-21=%u\n", -43%-21);
     printf("-126/-21=%u\n", -126/-21);
-    printf("-12%/-21=%u\n", -126%-21);
+    printf("-126%%-21=%u\n", -126%-21);
     printf("-131/-21=%u\n", -131/-21);
-    printf("-131%-21=%u\n", -131%-21);
+    printf("-131%%-21=%u\n", -131%-21);
     printf("-(UINT_MAX/2+3)/-2=%u\n", (0-(UINT_MAX/2+3))/-2);
-    printf("-(UINT_MAX/2+3)%-2=%u\n", (0-(UINT_MAX/2+3))%-2);
+    printf("-(UINT_MAX/2+3)%%-2=%u\n", (0-(UINT_MAX/2+3))%-2);
 }
 
 void macro_test(void)
