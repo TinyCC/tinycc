b0296139a8ca3c15cd601ca1449e6547c8a352a0
seyko <seyko2@gmail.com>
2016-04-13 05:17:13 +0300

    fix for the -dM patch
        assign fopen("/dev/null","w") to the s->ppfp insteed of NULL

--- tcc.c
+++ tcc.c
@@ -302,8 +302,13 @@ int main(int argc, char **argv)
                 tcc_error("could not write '%s'", s->outfile);
         }
         s->dffp = s->ppfp;
-        if (s->dflag == 'M')
-            s->ppfp = NULL;
+        if (s->dflag == 'M') {
+#ifndef TCC_TARGET_PE
+            s->ppfp = fopen("/dev/null", "w");
+#else
+            s->ppfp = fopen("nul", "w");
+#endif
+        }
     }
 
     tcc_set_output_type(s, s->output_type);
