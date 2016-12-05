TinyCC/tinycc

commit 6245db9fca4046e568da41c6a1f8c51ee9e2f56c

tccgen/32bits: fix unsigned long long -> int cast
gen_cast() failed to truncate long long's if they
were unsigned, which was causing mess on the vstack.

There was a similar bug here
    tccgen: 32bits: fix PTR +/- long long
    ed15cddacd145c74e4600af50f6616ba59ca0d8d

Both were not visible until this patch
    tccgen: arm/i386: save_reg_upstack
    b691585785086024549cfb9ac65f3397263965aa

I'd still assume that this patch is correct per se.

Also:
- remove 2x !nocode_wanted (we are already under a general
  "else if (!nocode_wanted)" clause above).

tccgen.c
@@ -2315,7 +2315,7 @@ static void gen_cast(CType *type)
                 }
 #if !defined(TCC_TARGET_ARM64) && !defined(TCC_TARGET_X86_64)
             } else if ((dbt & VT_BTYPE) == VT_LLONG) {
-                if ((sbt & VT_BTYPE) != VT_LLONG && !nocode_wanted) {
+                if ((sbt & VT_BTYPE) != VT_LLONG) {
                     /* scalar to long long */
                     /* machine independent conversion */
                     gv(RC_INT);
@@ -2343,7 +2343,7 @@ static void gen_cast(CType *type)
                        (dbt & VT_BTYPE) == VT_FUNC) {
                 if ((sbt & VT_BTYPE) != VT_LLONG &&
                     (sbt & VT_BTYPE) != VT_PTR &&
-                    (sbt & VT_BTYPE) != VT_FUNC && !nocode_wanted) {
+                    (sbt & VT_BTYPE) != VT_FUNC) {
                     /* need to convert from 32bit to 64bit */
                     gv(RC_INT);
                     if (sbt != (VT_INT | VT_UNSIGNED)) {
@@ -2373,7 +2373,7 @@ static void gen_cast(CType *type)
                 force_charshort_cast(dbt);
             } else if ((dbt & VT_BTYPE) == VT_INT) {
                 /* scalar to int */
-                if (sbt == VT_LLONG && !nocode_wanted) {
+                if ((sbt & VT_BTYPE) == VT_LLONG) {
                     /* from long long: just take low order word */
                     lexpand();
                     vpop();

commit e0fe69050d2df59da7370875c4f5eb557618e20c

arm64: Fix regression introduced by 6245db9.

tccgen.c
@@ -1197,6 +1197,7 @@ static int reg_fret(int t)
     return REG_FRET;
 }
 
+#if !defined(TCC_TARGET_ARM64) && !defined(TCC_TARGET_X86_64)
 /* expand 64bit on stack in two ints */
 static void lexpand(void)
 {
@@ -1217,6 +1218,7 @@ static void lexpand(void)
     }
     vtop[0].type.t = vtop[-1].type.t = VT_INT | u;
 }
+#endif
 
 #ifdef TCC_TARGET_ARM
 /* expand long long on stack */
@@ -2370,6 +2372,7 @@ static void gen_cast(CType *type)
                     tcc_warning("nonportable conversion from pointer to char/short");
                 }
                 force_charshort_cast(dbt);
+#if !defined(TCC_TARGET_ARM64) && !defined(TCC_TARGET_X86_64)
             } else if ((dbt & VT_BTYPE) == VT_INT) {
                 /* scalar to int */
                 if ((sbt & VT_BTYPE) == VT_LLONG) {
@@ -2380,6 +2383,7 @@ static void gen_cast(CType *type)
                 /* if lvalue and single word type, nothing to do because
                    the lvalue already contains the real type size (see
                    VT_LVAL_xxx constants) */
+#endif
             }
         }
     } else if ((dbt & VT_BTYPE) == VT_PTR && !(vtop->r & VT_LVAL)) {