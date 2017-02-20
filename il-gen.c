10551d6961f84f9b3c19441f0d3c7db666f07675
Michael Matz <matz@suse.de>
2017-02-20 01:39:48 +0100

    Clarify that the CIL target code is dead
    This backend doesn't work anymore since a long time.
    Don't remove it yet, it might be an inspiration.

--- il-gen.c
+++ il-gen.c
@@ -18,6 +18,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#error this code has bit-rotted since 2003
+
 /* number of available registers */
 #define NB_REGS             3
 
