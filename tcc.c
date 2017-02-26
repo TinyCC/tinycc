206829415a0040d8233ee36bb764ebe077d7571b
Christian Jullien <Christian Jullien>
2017-02-26 08:02:53 +0100

    Linux was incorrectly identified in banner when compiled on macOS Darwin

--- tcc.c
+++ tcc.c
@@ -160,6 +160,8 @@ static const char version[] =
         " Windows"
 #elif defined(__FreeBSD__) || defined(__FreeBSD_kernel__)
         " FreeBSD"
+#elif defined(__APPLE__)
+        " Darwin"
 #else
         " Linux"
 #endif
