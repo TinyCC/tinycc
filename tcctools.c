1b9935cf36e83b389cb26008df93a654af3f2074
Larry Doolittle <ldoolitt@recycle.lbl.gov>
2017-05-07 21:28:05 -0700

    Add missing const and add warning flags

--- tcctools.c
+++ tcctools.c
@@ -101,7 +101,7 @@ ST_FUNC int tcc_tool_ar(TCCState *s1, int argc, char **argv)
     char tfile[260], stmp[20];
     char *file, *name;
     int ret = 2;
-    char *ops_conflict = "habdioptxN";  // unsupported but destructive if ignored.
+    const char *ops_conflict = "habdioptxN";  // unsupported but destructive if ignored.
     int verbose = 0;
 
     i_lib = 0; i_obj = 0;  // will hold the index of the lib and first obj
