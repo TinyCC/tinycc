337dc84b57e9a901d62c81bfc946b3d073c66af6
Kurt Nalty <kurt.nalty@kurtnalty.com>
2019-01-09 16:53:50 +0000

    Fixed -static linking on x86_64 Linux

--- x86_64-gen.c
+++ x86_64-gen.c
@@ -625,11 +625,7 @@ static void gcall_or_jmp(int is_jmp)
     if ((vtop->r & (VT_VALMASK | VT_LVAL)) == VT_CONST &&
 	((vtop->r & VT_SYM) && (vtop->c.i-4) == (int)(vtop->c.i-4))) {
         /* constant symbolic case -> simple relocation */
-#ifdef TCC_TARGET_PE
 	greloca(cur_text_section, vtop->sym, ind + 1, R_X86_64_PC32, (int)(vtop->c.i-4));
-#else
-	greloca(cur_text_section, vtop->sym, ind + 1, R_X86_64_PLT32, (int)(vtop->c.i-4));
-#endif
         oad(0xe8 + is_jmp, 0); /* call/jmp im */
     } else {
         /* otherwise, indirect call */
