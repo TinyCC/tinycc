85483f321d8df6cce08ef88d86f50c9a60b307ef
Michael Matz <matz@suse.de>
2019-04-18 03:39:40 +0200

    Add 103_implicit_memmove testcase
    which checks that a declaration after TCC implicitely declares
    an internally used function (memmove here) doesn't create any problem.

---
+++ tests/tests2/103_implicit_memmove.c
@@ -0,0 +1,20 @@
+/* Test that the memmove TCC is emitting for the struct copy
+   and hence implicitely declares can be declared properly also
+   later.  */
+struct S { int a,b,c,d, e[1024];};
+int foo (struct S *a, struct S *b)
+{
+  *a = *b;
+  return 0;
+}
+
+void *memmove(void*,void*,long);
+void foo2 (struct S *a, struct S *b)
+{
+  memmove(a, b, sizeof *a);
+}
+
+int main()
+{
+  return 0;
+}
