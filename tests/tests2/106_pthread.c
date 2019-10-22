b073a9c1037cc2cd15306463f84e8daea3dbdb1f
herman ten brugge <hermantenbrugge@home.nl>
2019-10-22 20:43:30 +0200

    Add testcase for elf_version

---
+++ tests/tests2/106_pthread.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <errno.h>
+
+int
+main(void)
+{
+  int ret;
+  pthread_condattr_t attr;
+  pthread_cond_t condition;
+
+  pthread_condattr_init (&attr);
+  pthread_condattr_setpshared (&attr, PTHREAD_PROCESS_SHARED);
+  printf ("%s\n", pthread_cond_init (&condition, &attr) ? "fail":"ok");
+  pthread_condattr_destroy (&attr);
+  return 0;
+}
