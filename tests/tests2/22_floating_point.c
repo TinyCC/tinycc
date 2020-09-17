55b4754e84a5e801dea307f294115f3d1fe29ab0
Kyryl Melekhin <k.melekhin@gmail.com>
2020-09-17 07:12:49 +0000

    Revert "add tests for float conversions to u64"
    Because test's are not applicable in this case.
    
    This reverts commit a5e714abecfe6f3a94c7cdc231906191ce2491f1.

--- tests/tests2/22_floating_point.c
+++ tests/tests2/22_floating_point.c
@@ -14,12 +14,6 @@ test()
    }
 }
 
-double coerce(double x)
-{
-	x++;
-	return x;
-}
-
 int main()
 {
    // variables
@@ -61,31 +55,7 @@ int main()
    // type coercion
    a = 2;
    printf("%f\n", a);
-   printf("%f\n", coerce(2));
-
-   //type conversion to unsigned long int
-   float f = 3421.439;
-   double d = 7855.332231;
-   long double ld = 2469.346786989643234;
-
-   unsigned long int i;
-   i = f;
-   printf("%lu\n", i);
-   i = d;
-   printf("%lu\n", i);
-   i = ld;
-   printf("%lu\n", i);
-
-   f = -3421.439;
-   d = -7855.332231;
-   ld = -2469.346786989643234;
-   i = f;
-   printf("%lu\n", i);
-   i = d;
-   printf("%lu\n", i);
-   i = ld;
-   printf("%lu\n", i);
-
+   printf("%f\n", sin(2));
 
    return 0;
 }
