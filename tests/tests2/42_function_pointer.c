8f9bf3f223da01c200c6349ae2c2448e5b121161
herman ten brugge <hermantenbrugge@home.nl>
2020-09-21 09:18:48 +0200

    function pointer compare
    tccelf.c:
    - Check if symbol is in data section and UNDEF. Then generate new
      relocation and let dynamic linker solve it.
    
    tests/tests2/42_function_pointer.c:
    - Add new test code

--- tests/tests2/42_function_pointer.c
+++ tests/tests2/42_function_pointer.c
@@ -12,10 +12,18 @@ int (*f)(int) = &fred;
    (fprint here) must not be called directly anywhere in the test.  */
 int (*fprintfptr)(FILE *, const char *, ...) = &fprintf;
 
+typedef int (*func) (int);
+static int dummy1(int i) { return 0; }
+int dummy2(int i) { return 0; }
+static func allfunc[] = { putchar, dummy1, dummy2 };
+
 int main()
 {
    fprintfptr(stdout, "%d\n", (*f)(24));
 
+   printf ("%d\n", allfunc[0] == putchar);
+   printf ("%d\n", allfunc[1] == dummy1);
+   printf ("%d\n", allfunc[2] == dummy2);
    return 0;
 }
 
