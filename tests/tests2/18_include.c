0f72db09ab4bc9fb02b2a8981b1ae267dd4f649c
herman ten brugge <hermantenbrugge@home.nl>
2022-08-16 20:53:57 +0200

    Add testcase __has_include and __has_include_next

--- tests/tests2/18_include.c
+++ tests/tests2/18_include.c
@@ -6,6 +6,36 @@ int main()
 #include "18_include.h"
    printf("done\n");
 
+#define  INC   "18_include.h"
+
+#ifdef __has_include
+#if defined __has_include
+#if __has_include("18_include.h")
+   printf("has_include\n");
+#endif
+#if __has_include(INC)
+   printf("has_include\n");
+#endif
+#if __has_include("not_found_18_include.h")
+   printf("has_include not found\n");
+#endif
+#endif
+#endif
+
+#ifdef __has_include_next
+#if defined __has_include_next
+#if __has_include_next("18_include.h")
+   printf("has_include_next\n");
+#endif
+#if __has_include_next(INC)
+   printf("has_include_next\n");
+#endif
+#if __has_include_next("not_found_18_include.h")
+   printf("has_include_next not found\n");
+#endif
+#endif
+#endif
+
    return 0;
 }
 
