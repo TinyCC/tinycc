5b28165fbf8549c5d92893339e4f6b6181316894
herman ten brugge <hermantenbrugge@home.nl>
2023-07-05 19:28:58 +0200

    Fix test 131 for 32 bits targets

--- tests/tests2/131_return_struct_in_reg.c
+++ tests/tests2/131_return_struct_in_reg.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 
-#define DATA 0x1234567890abcd, 0x5a5aa5a5f0f00f0f
+#define DATA 0x1234567890abcdll, 0x5a5aa5a5f0f00f0fll
 
 struct s {
-	long int a;
-	long int b;
+	long long int a;
+	long long int b;
 };
 
 struct {
@@ -16,14 +16,14 @@ foo1(void)
 {
 	struct s d = { DATA };
 	struct s *p = &d;
-	long int x = 0;
+	long long int x = 0;
 	return *p;
 }
 
 struct s
 foo2(void)
 {
-	long int unused = 0;
+	long long int unused = 0;
 	return gp->d;
 }
 
@@ -31,7 +31,7 @@ struct s
 foo3(void)
 {
 	struct s d = { DATA };
-	long int unused = 0;
+	long long int unused = 0;
 	return d;
 }
 
@@ -40,10 +40,10 @@ main(void)
 {
 	struct s d;
 	d = foo1();
-	printf("%lx %lx\n", d.a, d.b);
+	printf("%llx %llx\n", d.a, d.b);
 	d = foo2();
-	printf("%lx %lx\n", d.a, d.b);
+	printf("%llx %llx\n", d.a, d.b);
 	d = foo3();
-	printf("%lx %lx\n", d.a, d.b);
+	printf("%llx %llx\n", d.a, d.b);
 	return 0;
 }
