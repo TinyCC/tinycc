c2b16069f4b874d770c798e175e530e17299de99
Lawrence Stubbs <lstubbs@techno.express>
2023-11-27 18:46:35 -0500

    update .cl_32.bat, .cl_64.bat and CMakeLists.txt

--- .cl_64.bat
+++ .cl_64.bat
@@ -3,4 +3,10 @@ if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools" (
     %comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 ) else if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community" (
     %comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+) else if EXIST "C:\Program Files\Microsoft Visual Studio\2022\BuildTools" (
+    %comspec% /k "C:\Program Files\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+) else if EXIST "C:\Program Files\Microsoft Visual Studio\2022\Community" (
+    %comspec% /k "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+) else if EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools" (
+    %comspec% /k "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 )
